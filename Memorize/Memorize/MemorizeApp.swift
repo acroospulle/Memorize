5fc1a9d58ecd63717eaf376e40760ce87ff818f2
acroospulle <Ashni.Croospulle@cix.csi.cuny.edu>
2023-02-20 12:17:54 -0500

    x

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ashni Croospulle on 2/20/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
