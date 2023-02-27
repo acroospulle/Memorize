d0da7873799355ede59fb9d6bf6a4cf589997547
acroospulle <Ashni.Croospulle@cix.csi.cuny.edu>
2023-02-27 13:19:11 -0500

    new proj

---
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,42 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Created by Ashni Croospulle on 2/27/23.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame: ObservableObject {
+
+    @Published private var model: MemoryGame<String>
+    private(set) var theme: EmojiMemoryTheme
+
+    init(theme: EmojiMemoryTheme? = nil) {
+        self.theme = theme ?? EmojiMemoryTheme.themes.randomElement()!
+        let emoji = self.theme.emoji.shuffled()
+        model = MemoryGame(numberOfPairsOfCards: self.theme.numberOfPairs) { emoji[$0] }
+    }
+
+    // MARK: - Access to the Model
+
+    var cards: [MemoryGame<String>.Card] {
+        model.cards
+    }
+
+    var score: Int {
+        model.score
+    }
+
+    // MARK: - Intent(s)
+
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+
+    func restart() {
+        let emoji = theme.emoji.shuffled()
+        let numberOfPairs: Int = theme.numberOfPairs
+        model = MemoryGame(numberOfPairsOfCards: numberOfPairs) { emoji[$0] }
+    }
+}
